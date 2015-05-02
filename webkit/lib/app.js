a83549784d339385845aabe92cc3b11099ab2e1a
volter9 <volter925@gmail.com>
2015-05-02 16:07:19 -0700

    Remove background.html

--- webkit/lib/app.js
+++ webkit/lib/app.js
@@ -2,9 +2,8 @@
 
 var App = {
     buildUI: function (handlePrivnoteLink) {
-        var elImText = document.getElementById('im_texts');
-        // TODO:
-
-        Privnote.crypt('hello world!', handlePrivnoteLink);
+        
+        
+        // Privnote.crypt('hello world!', handlePrivnoteLink);
     }
 };
